23473f98971e2fcf5fe4822353e3c1ce138243e0
Igor-creato <71209783+Igor-creato@users.noreply.github.com>
2022-12-08 19:04:56 +0300

    Trenirovka

---
+++ js/rating.js
@@ -0,0 +1,24 @@
+let test = document.querySelector('.page-h1');
+let test2 = document.querySelector('.page-font');
+let buttonClose = document.querySelector('.button-close');
+let nullString = document.createElement('div');
+    // nullString.className = 'null-string';
+let marginTopString = parseInt(getComputedStyle(test2, true) .marginTop);  
+let marginBottomString = parseInt(getComputedStyle(test2, true) .marginBottom);
+let heightString = test2.offsetHeight + marginTopString + marginBottomString;
+test.addEventListener('click', () => console.log('Меня кликнули'));
+test2.addEventListener('mouseover', () => console.log('мышь надомной'));
+buttonClose.addEventListener('click', () => {
+if (test2.style.display != 'none'){
+    test2.style.display = 'none';
+    test.after(nullString);
+    nullString.style.height = heightString+'px';
+}
+else{
+    test2.removeAttribute('style');
+    // test2.style.display = '';
+    nullString.remove('div');
+}})
+console.log(nullString);
+console.log(heightString);
+console.log(marginTopString);
